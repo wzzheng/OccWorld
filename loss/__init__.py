ae9e620b067637be96659c79a5fa36cecdf5e52d
Wenzhao Zheng <zhengwz18@mails.tsinghua.edu.cn>
2023-11-26 17:59:40 +0800

    add website

--- loss/__init__.py
+++
@@ -1,6 +0,0 @@
-from mmengine.registry import Registry
-OPENOCC_LOSS = Registry('openocc_loss')
-
-from .multi_loss import MultiLoss
-from .ce_loss import CeLoss
-from .plan_reg_loss_lidar import PlanRegLossLidar
--- model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .VAE.vae_2d_resnet import VAERes2D
-from .VAE.quantizer import VectorQuantizer
-
-from .transformer.pose_decoder import PoseDecoder
-from .transformer.pose_encoder import PoseEncoder
-from .transformer.PlanUtransformer import PlanUAutoRegTransformer
-
-from .TransVQVAE import TransVQVAE
